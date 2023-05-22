8377be77ec1ec7cd5f5ae7c62bbebb804a1e05dd
Mindaugas Ulonas <mindegi15@gmail.com>
2023-05-22 15:49:46 +0300

    fixing filter issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -21,26 +22,29 @@ class App extends Component {
   }
 
   render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+
     return (
     <div className="App">
 
       <input
-      className='search-box'
+        className='search-box'
         type='search'
         placeholder='Search monsters'
         onChange={(event) => {
-          const searchString = event.target.value.toLocaleLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchString);
-          })
+          const searchField = event.target.value.toLocaleLowerCase();
+          
           this.setState(() => {
-            return { monsters: filteredMonsters }
+            return { searchField: searchField }
           })
         }}
       />
 
       {
-        this.state.monsters.map((monster) => {
+        filteredMonsters.map((monster) => {
           return  <div key={monster.id}>
                     <h1>{ monster.name }</h1>
                   </div>
